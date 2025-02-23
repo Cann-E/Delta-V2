c1d3b5efb4c901e000b0b2f19934b9810ffa3130
asstrid123 <erin.astrid.b@gmail.com>
2025-02-23 16:46:40 -0600

    update

--- delta/urls.py
+++ delta/urls.py
@@ -3,9 +3,8 @@ from django.urls import path, include
 from .views import home_view, user_list_view, delete_user_view  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
-    path('users/', user_list_view, name='user_list'),  # View users
-    path('delete_user/<int:user_id>/', delete_user_view, name='delete_user'),  
-    path('accounts/', include('allauth.urls')),  
+    path('admin_dashboard/', admin_dashboard_view, name='admin_dashboard'),
+    path('developer_dashboard/', developer_dashboard_view, name='developer_dashboard'),
+    path('editor_dashboard/', editor_dashboard_view, name='editor_dashboard'),
+    path('basic_dashboard/', basic_dashboard_view, name='basic_dashboard'),
 ]
